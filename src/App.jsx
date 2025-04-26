ee66c5f6739bb272956ea3d4f071725a821ce699
Ashahab007 <ashahab007@gmail.com>
2025-04-26 21:13:21 +0600

    router, tailwind & daisyUi setup complete

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Explore Email & Password Authentication</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./layout/Root/Root.jsx";
+import Home from "./components/Home/Home.jsx";
+import "./App.css";
+
+const router = createBrowserRouter([
+  { path: "/", Component: Root, children: [{ index: true, Component: Home }] },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
